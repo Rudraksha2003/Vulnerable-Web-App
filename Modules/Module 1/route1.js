5819dcc45c7291332bae123dbf30319e28da613b
Rudraksh Gadekar <rudrakshaagadekar2003@gmail.com>
2023-12-26 18:35:02 +0530

    Updated to Module 1

--- Modules/Module 1/route1.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-
-// // Module 1 routes
-// router.get('/', (req, res) => res.sendFile(__dirname + '/module1.html'));
-// router.use('/dashboard', require('./dashboard/route_dashboard.js')); // Include dashboard routes
-
-// module.exports = router;
